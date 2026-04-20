901ce19456abff35942b5b02731aaa1af6f1c5e6
Abhinav Chavan <abhinav97chavan@gmail.com>
2026-04-21 01:00:35 +0530

    Update Assignment_18.c

--- Assignment_18.c
+++ Assignment_18.c
@@ -1,29 +1,32 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
 
-int main(){
+int main() {
     char str_1[20];
     char str_2[20];
     char reversed[20];
 
-    int length, i, compare;
+    int length = 0, i, j, compare = 0;
+    int isEqual = 1, isSubstring = 0;
 
-    // 1)Input
+
     printf("Enter a string: ");
     scanf("%s", str_1);
 
     printf("Enter another string for equality check: ");
     scanf("%s", str_2);
 
-    // 2)Length
-    length = strlen(str_1);
+    
+    while (str_1[length] != '\0') {
+        length++;
+    }
     printf("\nLength of string = %d\n", length);
 
-    // 3)Reversal
+    //  Reversal
     printf("Reversed string = ");
     i = length - 1;
-    int j = 0;
-    while(i >= 0){
+    j = 0;
+
+    while (i >= 0) {
         reversed[j] = str_1[i];
         printf("%c", str_1[i]);
         i--;
@@ -32,21 +35,55 @@ int main(){
     reversed[j] = '\0';
     printf("\n");
 
-    // 4)Palindrome
-    if(strcmp(str_1, reversed) == 0)
+    //  Palindrome
+    i = 0;
+    while (str_1[i] != '\0' && reversed[i] != '\0') {
+        if (str_1[i] != reversed[i]) {
+            isEqual = 0;
+            break;
+        }
+        i++;
+    }
+
+    if (isEqual == 1 && str_1[i] == '\0' && reversed[i] == '\0')
         printf("The string is a Palindrome\n");
     else
         printf("The string is NOT a Palindrome\n");
 
-    // 5)Equality
-    compare = strcmp(str_1, str_2);
-    if(compare == 0)
+    // 5) Equality 
+    i = 0;
+    isEqual = 1;
+
+    while (str_1[i] != '\0' || str_2[i] != '\0') {
+        if (str_1[i] != str_2[i]) {
+            isEqual = 0;
+            break;
+        }
+        i++;
+    }
+
+    if (isEqual == 1)
         printf("Strings are equal\n");
     else
         printf("Strings are not equal\n");
 
-    // 6)Substring
-    if(strstr(str_1, str_2) != NULL)
+    //  Substring
+    for (i = 0; str_1[i] != '\0'; i++) {
+        int k = i;
+        j = 0;
+
+        while (str_2[j] != '\0' && str_1[k] == str_2[j]) {
+            k++;
+            j++;
+        }
+
+        if (str_2[j] == '\0') {
+            isSubstring = 1;
+            break;
+        }
+    }
+
+    if (isSubstring == 1)
         printf("Substring found in main string\n");
     else
         printf("Substring NOT found in main string\n");
